f22841c68b3fca7ea2eca0536df6c33364bf31b4
Jesuscabado <jesuscabado@hotmail.com>
2023-03-14 10:09:09 +0100

    actualizada rama dev

--- menu.js
+++ menu.js
@@ -1,13 +1,22 @@
-fetch('https://api.edamam.com/api/recipes/v2?type=public&beta=true&app_id=2563a2b8&app_key=27f59cd8a60bdc17f0a1745ca62cdf5b&calories=2000')
-  .then(response => response.json())
-  .then(data => {
-    const platos = data.hits;
-    platos.forEach(hit => {
-      crearMenu(plato);
-    });
-    crearCarousel();
+let url = createBaseUrl();
 
-  })
-  .catch(error => {
-    console.log(error);
-  });
+function createBaseUrl(){
+  let url = new URL("https://api.edamam.com/api/recipes/v2")
+  url.searchParams.set("app_id" , "2563a2b8")
+  url.searchParams.set("app_key" , "27f59cd8a60bdc17f0a1745ca62cdf5b")
+  url.searchParams.set("type", "public")
+  url.searchParams.set("q", "meat")
+  url.searchParams.set("nutrients[ENERC_KCAL]", "2000")
+  return url;
+}
+
+fetch(url.toString()).then (response => response.json()).then (data => console.log(data))
+
+
+
+
+
+
+
+
+
